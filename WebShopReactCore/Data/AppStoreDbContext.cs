e84c32e6e233cbaa3bcf122f3cb4838867a8720d
ChrisJohDev <chris@chrisjohannesson.com>
2021-07-13 13:14:49 +0200

    Added classes/files AppUser.cs and AppStoreDbContext.cs

---
+++ WebShopReactCore/Data/AppStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WebShopReactCore.Data
+{
+    public class AppStoreDbContext:DbContext
+    {
+        public AppStoreDbContext(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ WebShopReactCore/Models/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShopReactCore.Models
+{
+    public class AppUser: ApplicationUser, IAppUser
+    {
+        public AppUser() : base() { }
+    }
+
+    public interface IAppUser
+    {
+
+    }
+
+}
