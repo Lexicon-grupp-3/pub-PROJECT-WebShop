f41e700138949ae1c0821e8cb0bd4988f7415778
Camilla <Camilla@ternegren.se>
2021-07-16 14:52:00 +0200

    Scaffolded Identity

---
+++ WebShopReactCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebShopReactCore.Data;
+using WebShopReactCore.Models;
+
+[assembly: HostingStartup(typeof(WebShopReactCore.Areas.Identity.IdentityHostingStartup))]
+namespace WebShopReactCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ WebShopReactCore/Controllers/AuthorBookController.cs
@@ -0,0 +1,94 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShopReactCore.Data;
+
+namespace WebShopReactCore.Controllers
+{
+    public class AuthorBookController : Controller
+    {
+        private readonly AppStoreDbContext _context;
+        public AuthorBookController(AppStoreDbContext ctx)
+        {
+            _context = ctx;
+        }
+        // GET: AuthorBookController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AuthorBookController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AuthorBookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AuthorBookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorBookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorBookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorBookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorBookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
