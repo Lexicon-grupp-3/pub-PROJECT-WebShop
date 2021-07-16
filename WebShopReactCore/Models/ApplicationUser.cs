f0ceb67c1b9007d4a2f6070b40b0b28bb832e3a8
Tom Birkeland <birkeland@live.se>
2021-07-16 09:43:13 +0200

    Test

--- WebShopReactCore/Models/ApplicationUser.cs
+++ WebShopReactCore/Models/ApplicationUser.cs
@@ -12,5 +12,10 @@ namespace WebShopReactCore.Models
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+
     }
 }
